b833ae3d0dcf217f07a2b9d2fa0fd6a608700476
aksan29 <fachrizal.aksan@gmail.com>
2024-11-22 09:36:46 +0100

    version 3 for file1.py

--- file1.py
+++ file1.py
@@ -3,3 +3,4 @@ print ('version 1')
 
 print(' this is version will be changed 1')
 print(' I want to make it in version 2 but only record file1.py')
+print ('now I making new line for version 3 and only record file.py again')
