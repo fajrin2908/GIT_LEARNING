2c986d1769d64e42a8bdf870be9d3672a97d17c1
fajrin2908 <fajrin.susan29@gmail.com>
2024-11-24 15:30:56 +0100

    version cloning

--- file1.py
+++ file1.py
@@ -2,3 +2,6 @@ print('git setup')
 print ('version 1')
 print ('new version 1')
 print ('add some statement here where we can say amend for the previous commit ')
+
+
+print('I will add this feature from the cloning')
